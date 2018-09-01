27eebd6bdb56ff05bc73d9501921aa8c7f94fa7c
EC2 Default User <ec2-user@ip-172-31-28-126.ap-southeast-1.compute.internal>
2018-09-01 07:14:30 +0000

    Generate the word model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180830074028) do
+ActiveRecord::Schema.define(version: 20180901070854) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20180830074028) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string "content"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_words_on_category_id"
   end
 
 end
