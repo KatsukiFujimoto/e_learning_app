9fb87f9675bd3af58e4714a87b7396a9e84d6f8f
EC2 Default User <ec2-user@ip-172-31-28-126.ap-southeast-1.compute.internal>
2018-09-08 08:53:33 +0000

    Create the lesson model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180901071630) do
+ActiveRecord::Schema.define(version: 20180908084743) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_lessons_on_category_id"
+    t.index ["user_id"], name: "index_lessons_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
